aff3e08a2c15e24e42baf48c2ca28938b16b3347
Alex, Lee <cine0831@gmail.com>
2022-10-23 22:06:36 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,25 +1,36 @@
 resource "aws_s3_bucket" "terraform_state_backend" {
   bucket = var.bucket
-  acl = var.s3_acl
 
   lifecycle {
-    prevent_destroy = true
-  }
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
+    prevent_destroy = false
   }
 
   tags = {
     Name = var.bucket
+  }
+}
+
+resource "aws_s3_bucket_acl" "terraform_state_backend_acl" {
+  bucket = aws_s3_bucket.terraform_state_backend.id
+  acl = var.s3_acl
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state_backend_versioning" {
+  bucket = aws_s3_bucket.terraform_state_backend.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_backend_encryption" {
+  bucket = aws_s3_bucket.terraform_state_backend.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      #kms_master_key_id = aws_kms_key.mykey.arn
+      #sse_algorithm     = "aws:kms"
+      sse_algorithm = "AES256"
+    }
   }
 }
 
@@ -33,7 +44,12 @@ resource "aws_dynamodb_table" "terraform_state_locks" {
     type = "S"
   }
 
+  lifecycle {
+    prevent_destroy = false
+  }
+
   tags = {
     Name = var.dynamodb_table
   }
 }
+
